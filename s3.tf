039b496deceee260feb8bee665683fb856f1e058
panwjoe <99195980+panwjoe@users.noreply.github.com>
2022-02-14 10:11:40 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -105,8 +105,11 @@ resource "aws_s3_bucket" "data_science" {
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
-    enabled = True
+    enabled = False
   }
+  
+  
+  
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "log/"
