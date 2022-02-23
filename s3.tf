c18a39ec0c20d1351fbc06d0c62f93c2e3bc638a
panwjoe <99195980+panwjoe@users.noreply.github.com>
2022-02-23 11:36:07 -0500

    Update s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- s3.tf
+++ s3.tf
@@ -132,7 +132,7 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
   versioning {
-    enabled = false
+    enabled = true
   }
   server_side_encryption_configuration {
     rule {
